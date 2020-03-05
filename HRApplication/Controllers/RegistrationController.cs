9866914ff556048194b04bc2cbbd6dcb48f3d5bd
Sai Rohith Reddy Vangala <sairohit.vangala@vannainfotech.com>
2020-03-06 01:26:40 +0530

    Created basic MVC App.
    Takes input of a new Resource and redirects to another page to display the details.
    Added default route attribute to HR Registration form.

---
+++ HRApplication/Controllers/RegistrationController.cs
@@ -0,0 +1,28 @@
+﻿using HRApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRApplication.Controllers
+{
+    [Route("")]
+    [Route("hr/registration")]
+    public class RegistrationController : Controller
+    {
+        [HttpPost("new")]
+        public IActionResult Register([Bind("ResourceName, Age, Department, Position, JoiningDate, Gender, HighestQualifications")] HumanResource newResource)
+        {
+            return RedirectToAction("ShowDetails", routeValues: newResource);
+        }
+
+        [HttpGet("details")]
+        public IActionResult ShowDetails(HumanResource resourceDetails)
+        {
+            return View(viewName: "ResourceDetails", model: resourceDetails);
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(viewName: "Form");
+        }
+    }
+}
---
+++ HRApplication/Models/HumanResource.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRApplication.Models
+{
+    public class HumanResource
+    {
+        [Required]
+        public string ResourceName { get; set; }
+        [Required]
+        public int Age { get; set; }
+        public long ResourceId { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public string Position { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime JoiningDate { get; set; }
+        public string HighestQualifications { get; set; }
+        [Required]
+        public char Gender { get; set; }
+    }
+}
